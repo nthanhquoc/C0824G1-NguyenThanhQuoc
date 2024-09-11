f3d904f2eb88367d4e1832e840ece9b2211c0831
nthanhquoc <nthanhquoc60@gmail.com>
2024-09-11 11:54:56 +0700

    Sua bai tap ss6

--- module1/ss6/baitap.js
+++ module1/ss6/baitap.js
@@ -1,17 +1,18 @@
 let diem_toan= document.getElementById('toan');
-
-
 let diem_van=document.getElementById('van');
-
 let diem_anh = document.getElementById('anh');
+let hien_thi=document.getElementById('hienthi');
+let btnButton=document.getElementById('btn');
+btnButton.addEventListener('click', function(){
+    let toan=parseFloat(diem_toan.value);
+    // alert(typeof toan)
+    let van =parseFloat(diem_van.value);
+    // alert(typeof van)
+    let anh = parseFloat(diem_anh.value);
+    // alert(typeof anh);
+    let avg= (toan+van+anh)/3;
+    alert('Diem Trung Binh La : '+avg);
+    hien_thi.innerHTML="Toan "+toan+"<br>"+"Van "+van+"<br>"+"Anh:"+anh+"<br>"+"Diem Trung Binh: "+avg;
 
-function tinhdiem(){
-    let toan=parseInt(diem_toan.value);
-    //alert(typeof toan)
-    let van =parseInt(diem_van.value);
-    //alert(typeof van)
-    let anh = parseInt(diem_anh.value);
-    //alert(typeof anh);
-     let avg= (toan+van+anh)/3;
-     alert('Diem Trung Binh La : '+avg);
-}
+
+})
