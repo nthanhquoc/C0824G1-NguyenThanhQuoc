63729da193ff9f377dd5a1dbedf11b9af91f99ab
nthanhquoc <nthanhquoc60@gmail.com>
2024-10-03 08:03:54 +0700

    Sua Bai Tap

--- module1/caseStudy/script.js
+++ module1/caseStudy/script.js
@@ -12,14 +12,10 @@ class Board {
     }
 
     removeList(listTitle) {
-        let updateList = [];
-        for (let i = 0; i < this.lists.length; i++) {
-            if (this.lists[i].title !== listTitle) {
-                updateList.push(this.lists[i]);
-            }
-        }
-        this.lists = updateList;
-        this.renderList()
+        this.lists = this.lists.filter(function (list) {
+            return list.title !== listTitle;
+        });
+        this.renderList();
     }
 
     renderList() {
@@ -30,16 +26,24 @@ class Board {
             let listDiv = document.createElement("div");
             listDiv.className = 'list';
             listDiv.innerHTML = `<h2>${list.title}</h2>`;
-            listDiv.addEventListener('dblclick', function () {
+
+            listDiv.addEventListener('dblclick', (function () {
                 let newListTitle = prompt("Input New List Title: ");
                 if (newListTitle) {
-                    let confirmList=confirm('Do You Want to Change List Title?');
-                    if(confirmList){
-                        list.title = newListTitle;
-                        board.renderList();
+                    let confirmList = confirm('Do You Want to Change List Title?');
+                    if (confirmList) {
+                        if (this.lists.some(function (item) {
+                            return item.title === newListTitle;
+                        })) {
+                            alert('A list with this title already exists!');
+                        } else {
+                            list.title = newListTitle;
+                            this.renderList();
+                        }
                     }
                 }
-            })
+            }).bind(this));
+
             for (let j = 0; j < list.cards.length; j++) {
                 let card = list.cards[j];
                 let cardDiv = document.createElement("div");
@@ -52,61 +56,73 @@ class Board {
                     <button class="setInfoCard"><img src="img/description.png" alt="Set InFo Card" style="width: 20px"></button>
                     <button class="removeCard"><img src="img/trash-can.png" alt="Remove Card" style="width: 20px"></button>
                 `;
-                cardDiv.querySelector('.viewCards').addEventListener('click', function () {
-                    board.viewCard(list.title, card.title);
-                })
-                cardDiv.querySelector('.addComments').addEventListener('click', function () {
+
+                cardDiv.querySelector('.viewCards').addEventListener('click', (function () {
+                    this.viewCard(list.title, card.title);
+                }).bind(this));
+
+                cardDiv.querySelector('.addComments').addEventListener('click', (function () {
                     let newComments = prompt('Enter a Comment ');
                     if (newComments) {
                         card.addComment(newComments);
                         alert('Comment Added Successfully');
                     }
-                })
-                cardDiv.querySelector('.setInfoCard').addEventListener('click', function () {
+                }).bind(this));
+
+                cardDiv.querySelector('.setInfoCard').addEventListener('click', (function () {
                     let inputText = prompt('1 to Change Card Title, 2 To Change Description');
                     if (inputText) {
-                        while(inputText<1||inputText>2||isNaN(inputText)||!parseInt(inputText)===inputText){
+                        while (inputText < 1 || inputText > 2 || isNaN(inputText) || !parseInt(inputText) === inputText) {
                             alert('Wrong Input!!!');
-                            inputText=prompt('1 to Change Card Title, 2 To Change Description');
+                            inputText = prompt('1 to Change Card Title, 2 To Change Description');
                         }
-                        if(inputText==1){
-                            let newCardTitle=prompt('Enter New Card Title:');
-                            card.title=newCardTitle;
-                            alert('Title Change Successfully!');
-                        }
-                        else{
-                            let newDescription=prompt('Enter New Description:');
+                        if (inputText == 1) {
+                            let newCardTitle = prompt('Enter New Card Title:');
+                            card.title = newCardTitle;
+                            alert('Title Changed Successfully!');
+                        } else {
+                            let newDescription = prompt('Enter New Description:');
                             card.setDescription(newDescription);
-                            alert('Description Change Successfully');
+                            alert('Description Changed Successfully');
                         }
                     }
-                    board.renderList();
-                })
-                cardDiv.querySelector('.removeCard').addEventListener('click', function () {
-                    let conFirmText = confirm("Do You Want To Delete Card ?");
-                    if (conFirmText) {
+                    this.renderList();
+                }).bind(this));
+
+                cardDiv.querySelector('.removeCard').addEventListener('click', (function () {
+                    let confirmText = confirm("Do You Want To Delete Card ?");
+                    if (confirmText) {
                         list.removeCard(card.title);
                     }
-                    board.renderList();
-                })
+                    this.renderList();
+                }).bind(this));
+
                 listDiv.appendChild(cardDiv);
             }
+
             let addCardButton = document.createElement('button');
             addCardButton.className = 'addCard';
             addCardButton.textContent = 'Add Card';
-            addCardButton.addEventListener('click', function () {
-                board.addCardToList(list);
-            })
+            addCardButton.addEventListener('click', (function () {
+                this.addCardToList(list);
+            }).bind(this));
+
             listDiv.appendChild(addCardButton);
             listContainer.appendChild(listDiv);
         }
-
     }
 
     addCardToList(list) {
         let cardTitle = prompt('Input Title Card: ');
-        let cardDescription = prompt('Input Description Card:  ');
         if (cardTitle) {
+            let existingCard = list.cards.find(function (card) {
+                return card.title === cardTitle;
+            });
+            if (existingCard) {
+                alert('A card with this title already exists!');
+                return;
+            }
+            let cardDescription = prompt('Input Description Card:  ');
             let newCard = new Card(cardTitle, cardDescription);
             list.addCard(newCard);
             this.renderList();
@@ -146,14 +162,14 @@ class Board {
     viewCard(listTitle, CardTitle) {
         let foundList = this.lists.find(function (item) {
             return item.title === listTitle;
-        })
+        });
         if (!foundList) {
             alert('List Not Found');
             return;
         }
         let foundCard = foundList.cards.find(function (item) {
             return item.title === CardTitle;
-        })
+        });
         if (!foundCard) {
             alert('Card Not Found');
             return;
@@ -180,12 +196,12 @@ class Board {
         let close = document.querySelector('.close');
         close.addEventListener('click', function () {
             modal.style.display = 'none';
-        })
-        window.onclick=function(event){
-            if(event.target===modal){
+        });
+        window.onclick = function (event) {
+            if (event.target === modal) {
                 modal.style.display = 'none';
             }
-        }
+        };
     }
 }
 
@@ -202,13 +218,9 @@ class List {
     }
 
     removeCard(cardTitle) {
-        let updateCard = [];
-        for (let i = 0; i < this.cards.length; i++) {
-            if (this.cards[i].title !== cardTitle) {
-                updateCard.push(this.cards[i]);
-            }
-        }
-        this.cards = updateCard;
+        this.cards = this.cards.filter(function (card) {
+            return card.title !== cardTitle;
+        })
     }
 }
 
@@ -256,16 +268,16 @@ class User {
 }
 
 let board = new Board('Quản Lý Bảng Công Việc');
-let removeListButton=document.querySelector('.removeList');
-let addListButton=document.querySelector('.addList');
-removeListButton.addEventListener('click', function(){
+let removeListButton = document.querySelector('.removeList');
+let addListButton = document.querySelector('.addList');
+removeListButton.addEventListener('click', function () {
     if (board.lists.length === 0) {
         alert('No Have List To Delete');
     } else {
         board.removeSelectList();
     }
-})
-addListButton.addEventListener('click',function(){
+});
+addListButton.addEventListener('click', function () {
     board.newAddList();
 })
 let todoList = new List('To Do');
