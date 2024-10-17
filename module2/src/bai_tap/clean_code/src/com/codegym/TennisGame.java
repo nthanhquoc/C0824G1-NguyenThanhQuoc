a88820e1ea4349eb6a6add647d2543485ca078dd
nthanhquoc <nthanhquoc60@gmail.com>
2024-10-17 16:09:16 +0700

    Lam Bai tap Clean Code

--- module2/src/bai_tap/clean_code/src/com/codegym/TennisGame.java
+++ module2/src/bai_tap/clean_code/src/com/codegym/TennisGame.java
@@ -2,62 +2,58 @@ package bai_tap.clean_code.src.com.codegym;
 
 public class TennisGame {
 
-    public static String getScore(String player1Name, String player2Name, int m_score1, int m_score2) {
-        String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                    score = "Love-All";
-                    break;
-                case 1:
-                    score = "Fifteen-All";
-                    break;
-                case 2:
-                    score = "Thirty-All";
-                    break;
-                case 3:
-                    score = "Forty-All";
-                    break;
-                default:
-                    score = "Deuce";
-                    break;
+    public static String getScore(String player1Name, String player2Name, int scorePlayer1, int scorePlayer2) {
+        if (scorePlayer1 == scorePlayer2) {
+            return getEqualScore(scorePlayer1);
+        } else if (isGameWon(scorePlayer1, scorePlayer2)) {
+            return getWinnerScore(scorePlayer1, scorePlayer2, player1Name, player2Name);
+        } else {
+            return getRegularScore(scorePlayer1, scorePlayer2);
+        }
+    }
 
-            }
+    private static String getEqualScore(int score) {
+        switch (score) {
+            case 0:
+                return "Love-All";
+            case 1:
+                return "Fifteen-All";
+            case 2:
+                return "Thirty-All";
+            case 3:
+                return "Forty-All";
+            default:
+                return "Deuce";
         }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+    }
+
+    private static boolean isGameWon(int scorePlayer1, int scorePlayer2) {
+        return scorePlayer1 >= 4 || scorePlayer2 >= 4;
+    }
+
+    private static String getWinnerScore(int scorePlayer1, int scorePlayer2, String player1Name, String player2Name) {
+        int scoreDifference = scorePlayer1 - scorePlayer2;
+
+        if (scoreDifference == 1) {
+            return "Advantage " + player1Name;
+        } else if (scoreDifference == -1) {
+            return "Advantage " + player2Name;
+        } else if (scoreDifference >= 2) {
+            return "Win for " + player1Name;
+        } else {
+            return "Win for " + player2Name;
         }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+    }
+
+    private static String getRegularScore(int scorePlayer1, int scorePlayer2) {
+        StringBuilder score = new StringBuilder();
+        String[] scoreNames = {"Love", "Fifteen", "Thirty", "Forty"};
+
+        for (int i = 1; i <= 2; i++) {
+            int currentScore = (i == 1) ? scorePlayer1 : scorePlayer2;
+            if (i == 2) score.append("-");
+            score.append(scoreNames[currentScore]);
         }
-        return score;
+        return score.toString();
     }
 }
