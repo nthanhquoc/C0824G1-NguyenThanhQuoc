5598e915ec0610aff678233b9bab055b51e55d78
nthanhquoc60 <nthanhquoc60@gmail.com>
2024-10-22 15:39:08 +0700

    Gui Bai Tap

--- module2/src/bai_tap/dsa/bt2/MyLinkedList.java
+++ module2/src/bai_tap/dsa/bt2/MyLinkedList.java
@@ -1,138 +1,13 @@
 package bai_tap.dsa.bt2;
 
-import bai_tap.dsa.bt1.MyList;
-
 public class MyLinkedList<E> {
     private Node head;
     private int numNodes;
 
     public MyLinkedList() {
         head = null;
-        numNodes = 0;
+        this.numNodes = 0;
     }
-
-    public void add(int index, E element) {
-        if (index < 0 || index > numNodes) {
-            throw new IndexOutOfBoundsException("index: " + index + " NumNode: " + numNodes);
-        }
-        if (index == 0) {
-            addFirst(element);
-        } else {
-            Node temp = head;
-            for (int i = 0; i < index; i++) {
-                temp = temp.next;
-            }
-            Node newNode = new Node(element);
-            newNode.next = temp.next;
-            temp.next = newNode;
-            numNodes++;
-        }
-    }
-
-    public void addFirst(E element) {
-        Node newNode = new Node(element);
-        newNode.next = head;
-        head = newNode;
-        numNodes++;
-    }
-
-    public void addLast(E element) {
-        Node newNode = new Node(element);
-        if (head == null) {
-            head = newNode;
-        } else {
-            Node current = head;
-            while (current.next != null) {
-                current = current.next;
-            }
-            current.next = newNode;
-        }
-        numNodes++;
-    }
-
-    public E remove(int index) {
-        if (index < 0 || index > numNodes) {
-            throw new IndexOutOfBoundsException("index: " + index + " NumNode: " + numNodes);
-        }
-        if (index == 0) {
-            Node temp = head;
-            head = head.next;
-            numNodes--;
-            return (E) temp.data;
-        } else {
-            Node previouse = head;
-            for (int i = 0; i < index; i++) {
-                previouse = previouse.next;
-            }
-            Node temp = previouse.next;
-            previouse.next = temp.next;
-            numNodes--;
-            return (E) temp.data;
-        }
-    }
-
-    public boolean remove(Object element) {
-        Node current = head;
-        Node previouse = null;
-        while (current != null) {
-            if (current.data.equals(element)) {
-                if (previouse == null) {
-                    head = head.next;
-                } else {
-                    previouse.next = current.next;
-                }
-                numNodes--;
-                return true;
-            }
-            previouse = current;
-            current = current.next;
-        }
-        return false;
-    }
-
-    public int size() {
-        return numNodes;
-    }
-
-    public MyLinkedList<E> clone() {
-        MyLinkedList<E> clonedList= new MyLinkedList<>();
-        Node current = head;
-        while (current != null) {
-            clonedList.addLast((E) current.data);
-            current = current.next;
-        }
-        return clonedList;
-    }
-
-    public boolean contains(E element) {
-        Node current = head;
-        while (current != null) {
-            if (current.data.equals(element)) {
-                return true;
-            }
-            current = current.next;
-        }
-        return false;
-    }
-    public int indexOf(E element) {
-        Node current = head;
-        int index = 0;
-        while (current != null) {
-            if (current.data.equals(element)) {
-                return index;
-            }
-            current = current.next;
-            index++;
-        }
-        return -1;
-    }
-    public boolean add(E element) {
-        
-    }
-    public void ensureCapacity(int minCapacity) {
-
-    }
-
 
     private class Node {
         private Node next;
@@ -146,4 +21,177 @@ public class MyLinkedList<E> {
             return data;
         }
     }
+
+    public void add(int index, E element) {
+        if (index < 0 || index > numNodes) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node newNode = new Node(element);
+        if (index == 0) {
+            addFirst(element);
+        } else {
+            Node prev = head;
+            for (int i = 0; i < index - 1; i++) {
+                prev = prev.next;
+            }
+            newNode.next = prev.next;
+            ;
+            prev.next = newNode;
+            this.numNodes++;
+        }
+    }
+
+    public void addFirst(E element) {
+        Node newNode = new Node(element);
+        newNode.next = head;
+        head = newNode;
+        this.numNodes++;
+
+    }
+
+    public void addLast(E element) {
+        Node newNode = new Node(element);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node prev = head;
+            while (prev.next != null) {
+                prev = prev.next;
+            }
+            prev.next = newNode;
+        }
+        this.numNodes++;
+    }
+
+    public E remove(int index) {
+        if (index < 0 || index > numNodes) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node temp = head;
+        if (index == 0) {
+            head = head.next;
+        } else {
+            Node pre = null;
+            for (int i = 0; i < index - 1; i++) {
+                pre = temp;
+                temp = temp.next;
+            }
+            pre.next = temp.next;
+        }
+        numNodes--;
+        return (E) temp.data;
+    }
+
+    public boolean remove(Object element) {
+        if (head == null) {
+            return false;
+        }
+        if (head.data.equals(element)) {
+            head = head.next;
+            numNodes--;
+            return true;
+        }
+        Node temp = head;
+        Node prev = null;
+        while (temp != null && !temp.data.equals(element)) {
+            prev = temp;
+            temp = temp.next;
+        }
+        if (temp != null) {
+            prev.next = temp.next;
+            numNodes--;
+            return true;
+        }
+        return false;
+    }
+
+    public int size() {
+        return numNodes;
+    }
+
+    public MyLinkedList<E> clone() {
+        MyLinkedList<E> clone = new MyLinkedList<>();
+        if(head==null){
+            return clone;
+        }
+        Node temp = head;
+        clone.addFirst((E) temp.data);
+        Node cloneTemp=clone.head;
+        temp=temp.next;
+        while(temp!=null){
+            cloneTemp.next=new Node(temp.data);
+            cloneTemp=cloneTemp.next;
+            temp=temp.next;
+        }
+        clone.numNodes=this.numNodes;
+        return clone;
+    }
+
+    public boolean contains(E o) {
+        Node temp = head;
+        while (temp != null) {
+            if (temp.data.equals(o)) {
+                return true;
+            }
+            temp = temp.next;
+        }
+        return false;
+    }
+
+    public int indexOf(E o) {
+        Node temp = head;
+        int index = 0;
+        while (temp != null) {
+            if (temp.data.equals(o)) {
+                return index;
+            }
+            temp = temp.next;
+            index++;
+        }
+        return -1;
+    }
+
+    public boolean add(E element) {
+        addLast(element);
+        return true;
+    }
+
+    public void ensureCapacity(int minCapacity) {
+
+    }
+
+    public E get(int index) {
+        if (index < 0 || index >= numNodes) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node temp = head;
+        for (int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+        return (E) temp.data;
+    }
+
+    public E getFirst() {
+        if(head==null){
+            return null;
+        }
+        return (E) head.data;
+    }
+
+    public E getLast() {
+        if(head==null){
+            return null;
+        }
+        Node temp=head;
+        while(temp.next!=null){
+            temp=temp.next;
+        }
+        return (E) temp.data;
+    }
+
+    public void clear() {
+        head = null;
+        numNodes = 0;
+    }
+
 }
--- module2/src/bai_tap/dsa/bt2/MyLinkedListTest.java
+++ module2/src/bai_tap/dsa/bt2/MyLinkedListTest.java
@@ -2,6 +2,35 @@ package bai_tap.dsa.bt2;
 
 public class MyLinkedListTest {
     public static void main(String[] args) {
-
+        MyLinkedList<String> myList = new MyLinkedList<>();
+        myList.addFirst("C");
+        myList.addLast("Java");
+        myList.addLast("Python");
+        myList.add(1, "C++");
+        System.out.println("Danh sách sau khi thêm: ");
+        DisplayList(myList);
+        System.out.println("Số lượng phần tử: " + myList.size());
+        System.out.println("Phần tử đầu tiên: " + myList.getFirst());
+        System.out.println("Phần tử cuối cùng: " + myList.getLast());
+        System.out.println("Danh sách có chứa Java không? " + myList.contains("Java"));
+        System.out.println("Danh sách có chứa Ruby không? " + myList.contains("Ruby"));
+        System.out.println("Xóa phần tử tại vị trí 2: " + myList.remove(2));
+        System.out.println("Danh sách sau khi xóa: ");
+        DisplayList(myList);
+        System.out.println("Xóa phần tử 'C++': " + myList.remove("C++"));
+        System.out.println("Danh sách sau khi xóa 'C++': ");
+        DisplayList(myList);
+        MyLinkedList<String> clonedList = myList.clone();
+        System.out.println("Bản sao của danh sách: ");
+        DisplayList(clonedList);
+        myList.clear();
+        System.out.println("Danh sách sau khi xóa toàn bộ: " + myList.size());
     }
+
+    private static void DisplayList(MyLinkedList<String> myList) {
+        for (int i = 0; i < myList.size(); i++) {
+            System.out.println(myList.get(i));
+        }
+    }
+
 }
