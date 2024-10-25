9bed26e301034674d0b1d74142d2092c47d98abb
nthanhquoc60 <nthanhquoc60@gmail.com>
2024-10-25 11:23:55 +0700

    Sua Bai Tap

--- module2/src/bai_tap/java_collection_framework/bt1/view/ProductView.java
+++ module2/src/bai_tap/java_collection_framework/bt1/view/ProductView.java
@@ -9,61 +9,62 @@ import java.util.Scanner;
 public class ProductView {
     public static void main(String[] args) {
         ProductController controller = new ProductController();
+        Scanner sc = new Scanner(System.in);
         int choice;
         int id;
         double price;
         String name;
-        displayMenu();
-        Scanner sc = new Scanner(System.in);
-        choice = inputValidation("choice", Integer.class, sc);
-        switch (choice) {
-            case 1:
-                controller.getAllProducts().forEach(System.out::println);
-                break;
-            case 2:
-                controller.addProduct(new Product(inputValidation("id", Integer.class, sc),
-                        inputValidation("name", String.class, sc),
-                        inputValidation("price", Double.class, sc)));
-                controller.getAllProducts().forEach(System.out::println);
-                break;
-            case 3:
-                id = inputValidation("id", Integer.class, sc);
-                Product product = controller.searchProduct(id);
-                if (product != null) {
-                    System.out.println(product);
-                } else {
-                    System.out.println("Product with id " + id + " not found.");
-                }
-                break;
-            case 4:
-                System.out.println("Sort Products To Prices");
-                controller.sortProducts();
-                controller.getAllProducts().forEach(System.out::println);
-                break;
-            case 5:
-                id = inputValidation("id", Integer.class, sc);
-                name = inputValidation("name", String.class, sc);
-                price = inputValidation("price", Double.class, sc);
+        do {
+            displayMenu();
+            choice = inputValidation("choice", Integer.class, sc);
+            switch (choice) {
+                case 1:
+                    controller.getAllProducts().forEach(System.out::println);
+                    break;
+                case 2:
+                    controller.addProduct(new Product(inputValidation("id", Integer.class, sc),
+                            inputValidation("name", String.class, sc),
+                            inputValidation("price", Double.class, sc)));
+                    controller.getAllProducts().forEach(System.out::println);
+                    break;
+                case 3:
+                    id = inputValidation("id", Integer.class, sc);
+                    Product product = controller.searchProduct(id);
+                    if (product != null) {
+                        System.out.println(product);
+                    } else {
+                        System.out.println("Product with id " + id + " not found.");
+                    }
+                    break;
+                case 4:
+                    System.out.println("Sort Products To Prices");
+                    controller.sortProducts();
+                    controller.getAllProducts().forEach(System.out::println);
+                    break;
+                case 5:
+                    id = inputValidation("id", Integer.class, sc);
+                    name = inputValidation("name", String.class, sc);
+                    price = inputValidation("price", Double.class, sc);
 
-                if (!controller.editProduct(id, name, price)) {
-                    System.out.println("Product with id " + id + " not found.");
-                }else{
-                    controller.getAllProducts().forEach(System.out::println);
-                }
-                break;
-            case 6:
-                id = inputValidation("id", Integer.class, sc);
-                if (!controller.deleteProduct(id)) {
-                    System.out.println("Product with id " + id + " not found.");
-                }
-                else {
-                    System.out.println("Delete Success");
-                    controller.getAllProducts().forEach(System.out::println);
-                }
-                break;
-            case 7:
-                break;
-        }
+                    if (!controller.editProduct(id, name, price)) {
+                        System.out.println("Product with id " + id + " not found.");
+                    } else {
+                        controller.getAllProducts().forEach(System.out::println);
+                    }
+                    break;
+                case 6:
+                    id = inputValidation("id", Integer.class, sc);
+                    if (!controller.deleteProduct(id)) {
+                        System.out.println("Product with id " + id + " not found.");
+                    } else {
+                        System.out.println("Delete Success");
+                        controller.getAllProducts().forEach(System.out::println);
+                    }
+                    break;
+                case 7:
+                    break;
+            }
+        } while (choice != 7);
     }
 
     private static void displayMenu() {
