68d84f273ee444c13888048d29a3da71835988cb
nthanhquoc60 <nthanhquoc60@gmail.com>
2024-11-20 17:08:51 +0700

    Gui bai Tap

---
+++ module3/baitap/bt3/bt1.sql
@@ -0,0 +1,15 @@
+use quanlysinhvien;
+
+select * from students where students.student_name like 'H%' or 'h%';
+select * from class where month(class.start_date)=12;
+select * from  subjects where subjects.credit between 3 and 5;
+
+SET SQL_SAFE_UPDATES = 0;
+update students set students.class_id=2 where students.student_name='Hung';
+SET SQL_SAFE_UPDATES = 1;
+
+select students.student_name,subjects.sub_name,marks.mark
+from students
+left join marks on students.student_id=marks.student_id
+left join subjects on marks.sub_id=subjects.sub_id
+order by marks.mark desc, students.student_name asc;
---
+++ module3/baitap/bt3/bt2.sql
@@ -0,0 +1,10 @@
+use quanlybanhang;
+insert into customers
+values(1,'Minh Quan',10),
+(2,'Ngoc Oanh',20),
+(3,'Hong Ha',50);
+
+insert into orders(o_id,c_id,o_date)
+values(1,1,'2006-3-11'),
+(2,2,'2006-3-23'),
+(3,1,'2006-3-16');
