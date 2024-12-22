0a1f28b30b36a21d576bb29777bce6031a475836
nthanhquoc60 <nthanhquoc60@gmail.com>
2024-12-22 21:05:56 +0700

    Thêm bài tâp

--- module4/bai_tap/bt6/blog/src/main/java/com/example/blog/controller/BlogController.java
+++ module4/bai_tap/bt6/blog/src/main/java/com/example/blog/controller/BlogController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/blogs")
@@ -17,8 +19,8 @@ public class BlogController {
     private IBlogService blogService;
 
     @GetMapping("")
-    public ModelAndView viewAllBLog(Model model) {
-        return new ModelAndView("list","blogs",blogService.getAll());
+    public ModelAndView viewAllBlog() {
+        return new ModelAndView("list", "blogs", blogService.getAll());
     }
 
     @GetMapping("/create")
@@ -26,9 +28,54 @@ public class BlogController {
         model.addAttribute("blog", new Blog());
         return "create";
     }
+
     @PostMapping("/create")
-    public String createBlog(Blog blog) {
+    public String createBlog(Blog blog, RedirectAttributes redirectAttributes) {
         blogService.save(blog);
+        redirectAttributes.addFlashAttribute("message", "Blog created successfully!");
         return "redirect:/blogs";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String viewUpdate(@PathVariable int id, Model model) {
+        Blog blog = blogService.findById(id);
+        model.addAttribute("blog", blog);
+        return "edit";
+    }
+
+    @PostMapping("/edit")
+    public String updateBlog(Blog blog, RedirectAttributes redirectAttributes) {
+        if (blogService.findById(blog.getId()) == null) {
+            redirectAttributes.addFlashAttribute("error", "Blog not found!");
+            return "redirect:/blogs";
+        }
+        blogService.update(blog.getId(), blog);
+        redirectAttributes.addFlashAttribute("message", "Blog updated successfully!");
+        return "redirect:/blogs";
+    }
+
+    @GetMapping("/{id}/delete")
+    public String viewDelete(@PathVariable int id, Model model) {
+        Blog blog = blogService.findById(id);
+        model.addAttribute("blog", blog);
+        return "delete";
+    }
+
+    @PostMapping("/delete")
+    public String deleteBlog(Blog blog, RedirectAttributes redirectAttributes) {
+        if (blogService.findById(blog.getId()) == null) {
+            redirectAttributes.addFlashAttribute("error", "Blog not found!");
+            return "redirect:/blogs";
+        }
+        blogService.remove(blog.getId());
+        redirectAttributes.addFlashAttribute("message", "Blog deleted successfully!");
+        return "redirect:/blogs";
+    }
+
+    @GetMapping("/{id}/view")
+    public String viewBlog(@PathVariable int id, Model model) {
+        Blog blog = blogService.findById(id);
+        model.addAttribute("blog", blog);
+        return "view";
     }
 }
